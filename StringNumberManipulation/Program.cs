fe15a2e64e661e403467e0d666672fda51e60c86
jim-conner <jamesdavidconner@gmail.com>
2021-07-20 21:30:54 -0500

    Add project files.

---
+++ StringNumberManipulation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StringNumberManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Please do stuff...");
+            //string userInput = Console.ReadLine();
+
+            //string[] str = { "1", "2", "3" };
+            string str = "1,2,3";
+            string[] splitStr = str.Split(",");
+
+
+            for (int i = 0; i < splitStr.Length; i++)
+            {
+                int.Parse(splitStr[i]);
+            }
+           
+            //int num = int.Parse(str);
+            Console.WriteLine(splitStr);
+
+        }
+    }
+}
